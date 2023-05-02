e92dab82b388e58aa52acaef608c997065f8b6a5
steven <admin@example.com>
2023-05-02 10:33:45 -0400

    restructured dir structure; created basic view

--- referral/marketplace/models.py
+++ referral/marketplace/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Marketplace(models.Model):
-    user = models.CharField(max_length=24)
-    request = models.CharField(max_length=12)
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    applicants = models.ManyToManyField(User, related_name='appliedPosts', null=True)
+    selectededApplicant = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='acceptedPosts', null=True)
+    postId = models.IntegerField(primary_key=True)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    postDate = models.DateTimeField()
+    requestPrice = models.IntegerField()
+    requestTypeChoices = (
+        ('WTB', 'Looking for referral'),
+        ('WTS', 'Offering referral')
+    )
+    requestType = models.CharField(max_length=3, choices=requestTypeChoices)
+
